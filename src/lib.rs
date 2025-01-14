5deb16b95e39f1d22df3b3a3ab292e2811a3ea3e
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2025-01-14 13:10:03 +1000

    skeleton for python bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 #[macro_use]
 extern crate ini;
 
+pub mod data_cache;
 pub mod hydrology;
 pub mod io;
 pub mod model;
 pub mod nodes;
-pub mod timeseries;
-pub mod timeseries_input;
 mod numerical;
+mod pybindings;
 pub mod tests;
 pub mod tid;
-pub mod data_cache;
+pub mod timeseries;
+pub mod timeseries_input;
 
 use pyo3::prelude::*;
 use rand::Rng;
@@ -51,12 +52,18 @@ fn guess_the_number() {
     }
 }
 
+#[pyfunction]
+pub fn load(s: &str) {
+    // TODO
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
 fn kalix(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(guess_the_number, m)?)?;
-
+    m.add_function(wrap_pyfunction!(load, m)?)?;
+    m.add_class::<pybindings::model::PyModel>()?;
     Ok(())
 }
---
+++ src/pybindings/model.rs
@@ -0,0 +1,39 @@
+use crate::model::Model;
+use numpy::PyArray;
+use pyo3::prelude::*;
+
+// https://pyo3.rs/v0.23.3/class.html#defining-a-new-class
+// This should be a wrapper for a Model, with methods calling Model's methods
+#[pyclass]
+pub struct PyModel {
+    model: Py<crate::model::Model>,
+}
+
+#[pymethods]
+impl PyModel {
+    // reload from str
+    fn reload(&mut self, s: &str) -> PyResult<()> {
+        // TODO implement
+        Ok(())
+    }
+
+    // run -> ()
+    fn run(&mut self) -> PyResult<()> {
+        // TODO implement
+        Ok(())
+    }
+
+    // get result names -> std::Vec<str> ?
+    fn get_result_names(&self, s: &str)
+    // TODO -> PyResult<&'py Vec<String>>
+    {
+        // TODO implement
+    }
+
+    // get result -> numpy array
+    fn get_result(&self, s: &str)
+    // TODO -> PyResult<&'py PyArray<f64, ndarray::Dim<[usize; 1]>>>
+    {
+        // TODO implement
+    }
+}
